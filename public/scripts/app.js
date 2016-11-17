a563db4bb8c2305600f991a7ac037886d4870088
Nathan Eyerley <eyerleyn@gmail.com>
2016-11-17 02:08:01 +0000

    finshed rough draft of dynamic tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,60 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
 
+$(function () {
+
+ function createTweetElement() {
+   var name = tweetData.user.name
+   var avatar = tweetData.user.avatars.small
+   var handle = tweetData.user.handle
+   var content = tweetData.content.text
+   var created = tweetData.created_at
+   var $tweet = $("<article>").addClass("tweet");
+   var $header = $('<header>');
+   var $footer = $('<footer>');
+   $tweet.append($header)
+   var $name = $('<h2>').addClass('.head-text').text(name);
+   var $handle = $('<span>').addClass(".userhandle").text(handle);
+   var $avatar = $('<img>').addClass(".avatar").attr("src", avatar)
+   $header.append($handle).append($name).append($avatar)
+   $('#created-tweets').append($tweet)
+   var $tweettext = $('<p>').addClass(".tweet-text").text(content)
+   $tweet.append($tweettext)
+   $footer.append($foottext)
+   var $foottext = $('<p>').addClass(".foot-text").text(created)
+   $tweet.append($foottext)
+   // return $tweet
+   // // }
+   // return $tweet
+   console.log($tweet[0])
+ }
+
+
+
+
+
+
+
+
+
+var tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": {
+      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+    },
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
+
+var $tweet = createTweetElement(tweetData);
+
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+$('#tweets-container').append($tweet);
+
+});
