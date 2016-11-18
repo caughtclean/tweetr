bad98b7a8f006bb2644d25aa44ad7514740e4cec
Nathan Eyerley <eyerleyn@gmail.com>
2016-11-18 21:15:14 +0000

    Fix mongo connection string issue

--- server/index.js
+++ server/index.js
@@ -1,11 +1,10 @@
-
-"use strict";
+require('dotenv').config();
 var cool = require('cool-ascii-faces');
-const PORT          = 8080;
-const express       = require("express");
-const bodyParser    = require("body-parser");
-const app           = express();
-const MONGODB_URI   = "mongodb://localhost:27017/tweeter";
+const PORT = 8080;
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const MONGODB_URI = process.env.MONGODB_URI;
 const db = require("mongodb");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -13,23 +12,20 @@ app.use(express.static("public"));
 
 
 db.connect(MONGODB_URI, (err, db) => {
- if (err) {
-   console.error(`Failed to connect: ${MONGODB_URI}`);
-   throw err;
- }
+  if (err) {
+    console.error(`Failed to connect: ${MONGODB_URI}`);
+    throw err;
+  }
 
-const DataHelpers = require("./lib/data-helpers.js")(db);
+  const DataHelpers = require("./lib/data-helpers.js")(db);
 
-const tweetsRoutes = require("./routes/tweets")(DataHelpers);
+  const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
-app.get('/cool', function(request, response) {
-  response.send(cool());
-});
 
-app.use("/tweets", tweetsRoutes);
+    app.use("/tweets", tweetsRoutes);
 
-app.listen(PORT, () => {
- console.log("Example app listening on port " + PORT);
-});
+    app.listen(PORT, () => {
+      console.log("Example app listening on port " + PORT);
+    });
+  });
 
-})
